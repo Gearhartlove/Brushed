b483412cf1fb849ad8893193bb873afc209fd588
Gearhartlove <kristoffgearhartfinley@gmail.com>
2022-07-15 19:55:15 -0700

    Controls Setup.

---
+++ Assets/Scripts/Controls.cs
@@ -0,0 +1,34 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Controls : MonoBehaviour
+{
+    public void OnW() {
+        Debug.Log("Pressed W");
+    }
+
+    public void OnA() {
+        Debug.Log("Pressed A"); 
+    }
+    
+    public void OnS() {
+        Debug.Log("Pressed S");
+    }
+    
+    public void OnD() {
+        Debug.Log("Pressed D");
+    }
+    
+    public void OnQ() {
+        Debug.Log("Pressed Q");
+    }
+    
+    public void OnE() {
+        Debug.Log("Pressed E");
+    }
+
+    public void OnEscape() {
+        Debug.Log("Pressed Escape");
+    }
+}
